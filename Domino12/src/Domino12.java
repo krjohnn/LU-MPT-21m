afa0632c3471c7b1fad9dd7b14bab0b8faeebfaa
krjohnn <ks91ko@gmail.com>
2018-10-11 09:35:10 +0300

    Generating pieces

--- Domino12/src/Domino12.java
+++ Domino12/src/Domino12.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
 
 public class Domino12 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x;
-		x = Integer.parseInt(args[0]); // Input param, player count
-
+		int x, c;
+		/* Temporary input */
+		
+		Scanner scanner = new Scanner(System.in);
+//		x = Integer.parseInt(scanner.nextLine());
+		x = scanner.nextInt();
+		
+//		x = Integer.parseInt(args[0]); // Input param, player count
+		c = 12;
+		System.out.println("a \n");
+		for(int i=0; i<=12; i++, c--) {
+			for(int j=i; j<=12; j++) {
+				System.out.println(j+"-"+i+"\n");
+			}
+//			System.out.println(i + " i \n");
+//			System.out.println(c + " c \n");
+		}
+		return;
 	}
 
 }
