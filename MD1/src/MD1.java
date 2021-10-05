e9e2f0978dd7f16414bad29a36cf40b6959e49ef
krjohnn <ks91ko@gmail.com>
2021-10-05 18:34:23 +0300

    Update MD1.java
    Updated logic to MD1 2021 version

--- MD1/src/MD1.java
+++ MD1/src/MD1.java
@@ -1,10 +1,10 @@
 /*
  * Autors
- * 
+ *
  * Vārds: Krišjānis
  * Uzvārds: Šulcs
  * Studentu apliecības numurs: ks10163
- * 
+ *
  */
 
 /*
@@ -12,16 +12,16 @@
  * ------------
  * Top = N+Z-1 spaces (maxSpaces)
  * Bottom = N spaces (maxPluses)
- * 
+ *
  * Pluses
  * ------------
- * Top = 1
- * Bottom = Z pluses
- * 
+ * Top = Z pluses
+ * Bottom = 1
+ *
  * Newlines
  * ------------
  * Equal to pluses
- * 
+ *
  */
 
 import java.util.Scanner;
@@ -32,24 +32,25 @@ public class MD1 {
 		int Z,N;
 		Z = Integer.parseInt(args[0]); // Input param, count of "+"
 		N = Integer.parseInt(args[1]); // Input param, count of " " from left
-		
+
 		/* Temporary input */
-		
-//		Scanner scanner = new Scanner(System.in);
-//		Z = Integer.parseInt(scanner.nextLine());
-//		N = Integer.parseInt(scanner.nextLine());
-//		Z = scanner.nextInt();
-//		N = scanner.nextInt();
-		
+
+//        Scanner scanner = new Scanner(System.in);
+//        Z = Integer.parseInt(scanner.nextLine());
+//        N = Integer.parseInt(scanner.nextLine());
+//        Z = scanner.nextInt();
+//        N = scanner.nextInt();
+
 		if((Z <= 0 || Z >= 20) || (N <= 0 || N >= 30)) {
 			System.out.println("DATI NAV KOREKTI!");
 			return;
-		}		
-		
-		int maxSpaces = N+Z-1; // Topmost "+" will not have more than N+Z-1 spaces
-		int maxPluses = 1; // Count of topmost "+"
+		}
+
+		int maxSpaces = N; // Topmost "+" will not have more than N spaces
+		int maxPluses = Z; // Count of topmost "+"
 		int maxNewlines = Z; // We will print as many newlines as there are "+"
-		
+
+		System.out.println("1234567890123456789012345678901234567890123456789012345678901234567890");
 		for(int i = 0; maxNewlines > i; i++) {
 			for(int j = 0; maxSpaces > j; j++) {
 				System.out.print(" ");
@@ -58,10 +59,10 @@ public class MD1 {
 				System.out.print("+ ");
 			}
 			System.out.print("\n");
-			maxSpaces--;
-			maxPluses++;
+			maxSpaces++; // Increase the spaces we need to add as we go "down"
+			maxPluses--; // Go "down" the count of pluses
 		}
-		System.out.println("1234567890123456789012345678901234567890123456789012345678901234567890");
+
 		return;
 	}
 
@@ -69,10 +70,25 @@ public class MD1 {
 
 /* EXAMPLE OUTPUT
 
-           +
-          + +
-         + + +
-        + + + +
 1234567890123456789012345678901234567890123456789012345678901234567890
+                             + + + + + + + + + + + + + + + + + + +
+                              + + + + + + + + + + + + + + + + + +
+                               + + + + + + + + + + + + + + + + +
+                                + + + + + + + + + + + + + + + +
+                                 + + + + + + + + + + + + + + +
+                                  + + + + + + + + + + + + + +
+                                   + + + + + + + + + + + + +
+                                    + + + + + + + + + + + +
+                                     + + + + + + + + + + +
+                                      + + + + + + + + + +
+                                       + + + + + + + + +
+                                        + + + + + + + +
+                                         + + + + + + +
+                                          + + + + + +
+                                           + + + + +
+                                            + + + +
+                                             + + +
+                                              + +
+                                               +
 
 */
